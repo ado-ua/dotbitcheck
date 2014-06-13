f095bcea4e3dc3f8380fb5c550d1ec91542afee8
ado-ua <github@goroshko.net>
2014-06-13 15:43:45 +0300

    Colorising

--- dotbitchecker/dotbitchecker.py
+++ dotbitchecker/dotbitchecker.py
@@ -1,5 +1,20 @@
 """
 Script to check status of dotbit accounts
+Copyright (C) 2014 ado-ua
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import urllib
@@ -82,16 +97,33 @@ def main(sessions, loop):
             session_index = session_index + 1
         for response in new_responses:
             if response == old_responses[response_index]:
-                print response[0], "not updated"
+                print response[0].rstrip(), "not updated"
                 response_index = response_index + 1
             else:
                 old_responses[response_index] = response
-                print response[0], "updated"
-                print "BTC: ", float(response[1]), "LTC: ", float(response[2]), "PPC: ", float(response[3]), "XMP: ", float(response[4])
+                print response[0].rstrip(), "updated"
+                if float(response[1]) > 0:
+                    btc = "\033[92m" + str(round(float(response[1]),1)) + "\033[0m"
+                else:
+                    btc = float(response[1])
+                if float(response[2]) > 0:
+                    ltc = "\033[92m" + str(round(float(response[2]),1)) + "\033[0m"
+                else:
+                    ltc = float(response[2])
+                if float(response[3]) > 0:
+                    ppc = "\033[92m" + str(round(float(response[3]),1)) + "\033[0m"
+                else:
+                    ppc = float(response[3])
+                if float(response[4]) > 0:
+                    xmp = "\033[92m" + str(round(float(response[4]),1)) + "\033[0m"
+                else:
+                    xmp = float(response[4])
+                print "BTC: ", btc, "LTC: ", ltc, "PPC: ", ppc, "XMP: ", xmp
                 response_index = response_index + 1
         if loop == "False":
             break
 
+# Construction of array based on input given
 if opts.file:
     sessions = list()
     file_handler = open(opts.file)
@@ -100,5 +132,4 @@ if opts.file:
 else:
     sessions = (opts.session,)
 
-print opts.loop
 main(sessions,opts.loop)
